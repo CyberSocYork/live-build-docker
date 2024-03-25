68173f645bc02b9b5190ed761e887dae5ff06233
Jacob Turner <jacob11turner@gmail.com>
2024-03-25 11:32:18 +0000

    Update submodule

--- SeeCacheAccess.sh
+++ SeeCacheAccess.sh
@@ -1,1 +1,1 @@
-docker exec -it $(sudo docker container ls | grep "live-build/live-build-container" | cut -d' ' -f 1,2) tail -f /var/log/apt-cacher-ng/apt-cacher.log
+docker exec -it $(docker container ls | grep "live-build/live-build-container" | cut -d' ' -f 1,2) tail -f /var/log/apt-cacher-ng/apt-cacher.log
